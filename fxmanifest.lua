eb2d4a38b6e27d7b372328d6c1352c3429989662
Sizzle <93177291+Sizzle-Z@users.noreply.github.com>
2022-01-23 14:59:26 -0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+
+author 'Sizzle#0007'
+description 'Patched Weapon Multipliers'
+version '1.1'
+
+files {
+	'data/weapons.meta',
+	'data/weaponcomponents.meta',
+	'data/weaponanimations.meta',
+    'data/pedhealth.meta',
+}
+
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/weaponanimations.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/weaponcomponents.meta'
+data_file 'PED_HEALTH_FILE' 'data/pedhealth.meta'
---
+++ server.lua
@@ -0,0 +1,5 @@
+Citizen.CreateThread(function()
+	Citizen.Wait(5000)
+	
+	print ('Weapon damage patched by Sizzle#0007')
+end)
